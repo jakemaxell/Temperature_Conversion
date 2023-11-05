98e301d6517e18a57c78942c6ba18de523e08d4c
Jacob Maxwell <jmaxwell@tocafootball.com>
2023-11-04 21:40:07 -0400

    Added temperature conversion logic

--- App.js
+++ App.js
@@ -1,12 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
-import { Button, SafeAreaView, StyleSheet, Text, TextInput, View } from 'react-native';
+import { Button, SafeAreaView, StyleSheet, Text, TextInput, View, Switch } from 'react-native';
 import React, { useState } from 'react';
 
 export default function App() {
-  const [inputValue, setInputValue] = useState('');
-  const convertFromFahrenheitToCelsius = () => {
-    let celsius = (inputValue - 32) * (5/9);
-    alert(inputValue + " degrees Fahrenheit is " + celsius + " degrees Celsius!")
+  const [temperature, setTemperature] = useState('');
+  const [isEnabled, setIsEnabled] = useState(false); 
+  const [temperatureType, setTemperatureType] = useState('Fahrenheit');
+
+  const toggleSwitch = () => {
+    setIsEnabled(previousState => !previousState);
+    isEnabled ? setTemperatureType("Fahrenheit") : setTemperatureType("Celsius");
+  };
+
+  const conversion = () => {
+    console.log(temperatureType);
+
+    if(temperature === ""){
+      alert("No temperature data was entered!!!");
+    }
+
+    if(temperatureType == "Fahrenheit"){
+      let celsius = ((temperature - 32) * (5/9)).toFixed(0);
+      alert(temperature + " degrees Fahrenheit is " + celsius + " degrees Celsius!");
+    } 
+    else if(temperatureType == "Celsius"){
+      let fahrenheit = ((temperature * (9/5) + 32)).toFixed(0);
+      alert(temperature + " degrees Celsius is " + fahrenheit + " degrees Fahrenheit!");
+    }
+
+    setTemperature("");
   };
 
   return (
@@ -14,9 +36,22 @@ export default function App() {
       <View style={{flex: 1, width: "100%", alignItems: 'center', backgroundColor: "#4785EA", justifyContent: 'center'}}>
         <Text style={{fontSize: 25, fontWeight: "bold", width: "100%", textAlign: 'center'}}>Temperature Conversion App</Text>
       </View>
-      <View style={{backgroundColor: "#47C7EA", flex: 3}}>
-        <TextInput style={styles.input} placeholder='Enter your temperature' value={inputValue} onChangeText={text => setInputValue(text)}/>
-        <Button onPress={convertFromFahrenheitToCelsius} title='Convert'/>
+      <View style={{backgroundColor: "#47C7EA", flex: 5}}>
+        <TextInput style={styles.input} placeholder={"Enter your temperature in " + temperatureType} value={temperature} onChangeText={text => setTemperature(text)}/>
+        <View style={{flexDirection: 'row', alignItems: 'center'}}>
+          <View style={{flex: 1, padding: 20, justifyContent: 'center'}}>
+            <Switch
+            trackColor={{ false: 'grey', true: 'green' }}
+            thumbColor={isEnabled ? 'white' : 'white'}
+            ios_backgroundColor='grey'
+            onValueChange={toggleSwitch}
+            value={isEnabled}
+          />
+          </View>
+          <View style={{flex: 1, justifyContent: 'center'}}>
+            <Button onPress={conversion} title='Convert' color="black"/>
+          </View>
+        </View>
       </View>
     </SafeAreaView>
   );
